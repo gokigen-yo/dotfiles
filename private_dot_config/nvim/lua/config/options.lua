efd86dbe4a343f840f3ae742c5ab19806bce4366
gokigen-yo <kntono.534@gmail.com>
2026-02-21 22:18:01 +0900

    add neovim settings

---
+++ private_dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,9 @@
+-- Default options: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+
+-- 表示言語を日本語にする
+vim.cmd("language ja_JP.UTF-8")
+
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+vim.opt.whichwrap = "b,s,h,l,<,>,[,]"
